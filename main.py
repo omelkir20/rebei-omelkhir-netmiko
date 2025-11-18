ca73c37bff5e8f57025e61ecb3e8490499681995
omelkir20 <omelkirrebei22@gmail.com>
2025-11-18 09:18:31 +0100

    Ajout de la fonction acces_netmiko

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# main.py
+from netmiko import ConnectHandler
+def acces_netmiko():
+    cisco_r = {
+        "device_type": "cisco",
+        "host": "sandbox-iosxr-1.cisco.com",
+        "username": "admin",
+        "password": "C1sco12345",
+        "port": 22,
+    }
 print("Hello, Git!")
+dire_bonjour()
